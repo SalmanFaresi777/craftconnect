888d6426006232a312f10d1395dbd63f26df5c6f
maishamomtaz <138659306+maishamomtaz@users.noreply.github.com>
2025-03-02 23:38:52 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+//import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
@@ -9,27 +10,42 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  //Link
 } from "react-router-dom";
 import Login from './screens/Login';
 import SignUp from './screens/SignUp';
-import MyOrders from './screens/MyOrders';
 import { CartProvider } from './components/ComponentReducer.js';
+import MyOrder from './screens/MyOrder';
+import PaymentSuccess from './screens/PaymentSuccess';
+import PaymentFail from './screens/PaymentFail';
+import PaymentCancel from './screens/PaymentCancel';
 
 function App() {
   return (
     <CartProvider>
+
       <Router>
         <div>
+
           <Routes>
+
             <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/creatuser" element={<SignUp />} />
-            <Route exact path="/myorders" element={<MyOrders />} />
+            <Route exact path="/myOrder" element={<MyOrder />} />
+            <Route exact path="/payment-success" element={<PaymentSuccess />} />
+            <Route exact path="/payment-fail" element={<PaymentFail />} />
+            <Route exact path="/payment-cancel" element={<PaymentCancel />} />
+
           </Routes>
+
         </div>
       </Router>
-    </CartProvider>
+
+      </CartProvider>
+   
+
   );
 }
 
